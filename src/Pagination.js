1614858e5e5205c40f587a6332458da4c946d9cc
marwa mazini <marwa6mzi@gmail.com>
2023-11-12 23:22:01 +0100

    message

--- src/Pagination.js
+++ src/Pagination.js
@@ -2,42 +2,37 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 function Api() {
-    const [info, setInfo] = useState([]);
-    const [page, setPage] = useState(1);
+  const [todos, setTodos] = useState([]);
+  const [page, setPage] = useState(1);
 
-    useEffect(() => {
-        axios.get(`https://jsonplaceholder.typicode.com/todos?_page=${page}&_limit=10`)
-            .then(response => setInfo(response.data))
-            .catch(error => console.error(error));
-    }, [page]);
+  useEffect(() => {
+    axios.get(`https://jsonplaceholder.typicode.com/todos?_page=${page}&_limit=10`)
+      .then(response => setTodos(response.data))
+      .catch(error => console.error(error));
+  }, [page]);
 
-    const handlePreviousPage = () => {
-        if (page > 1) {
-            setPage(page - 1);
-        }
-    };
+  const handlePreviousPage = () => {
+    if (page > 1) {
+      setPage(page - 1);
+    }
+  };
 
-    const handleNextPage = () => {
-        setPage(page + 1);
-    };
+  const handleNextPage = () => {
+    setPage(page + 1);
+  };
 
-    return (
-        <div className="App">
-            <h1>Todo List</h1>
-            <ul>
-                {Array.isArray(info) && info.length > 0 ? (
-                    info.map(todo => (
-                        <li key={todo.id}>{todo.title}</li>
-                    ))
-                ) : (
-                    <p>No todos found.</p>
-                )}
-            </ul>
-
-            <button onClick={handlePreviousPage}>Précédent</button>
-            <button onClick={handleNextPage}>Suivant</button>
-        </div>
-    );
+  return (
+    <div className="App">
+      <h1>Todo List</h1>
+      <ul>
+        {todos.map(todo => (
+          <li key={todo.id}>{todo.title}</li>
+        ))}
+      </ul>
+      <button onClick={handlePreviousPage}>Précédent</button>
+      <button onClick={handleNextPage}>Suivant</button>
+    </div>
+  );
 }
 
 export default Api;
